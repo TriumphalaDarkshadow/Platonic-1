6ff5166c37c668f3128297fad1bfe5724b87bcc6
TriumphalaDarkshadow <34397221+TriumphalaDarkshadow@users.noreply.github.com>
2017-12-13 22:19:42 +0100

    Update CreateServlet3.java
    CreateServlet3 heißt jetzt UserCreationServlet

--- platonic_snake/src/user/CreateServlet3.java
+++ platonic_snake/src/user/CreateServlet3.java
@@ -1,69 +1,42 @@
 package user;
 
-import java.sql.*;
-
+import java.awt.Image;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javax.annotation.Resource;
-// import javax.resource.cci.ResultSet;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 import javax.sql.DataSource;
 
- 
-//Neuen Eintrag anlegen
 /**
- * Servlet implementation class CreateServlet
+ * Servlet implementation class UserCreationServlet
  */
-@WebServlet("/CreateServlet3")
-public class CreateServlet3 extends HttpServlet {
+@WebServlet("/UserCreationServlet")
+@MultipartConfig
+
+public class UserCreationServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
- 
+    
     @Resource(lookup="jdbc/MyTHIPool")
-    private DataSource ds; 
-       
+    private DataSource ds;
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CreateServlet3() {
+    public UserCreationServlet() {
         super();
         // TODO Auto-generated constructor stub
-    }
-
-    public void persist(UserData form) throws ServletException {
-        String [] generatedKeys = new String [] {"id"};
-        try (Connection con = ds.getConnection(); 
-                final Statement stmt = con.createStatement();
-                PreparedStatement pstmt = con.prepareStatement(
-                        "INSERT INTO Users (username, email, password, birthday, location, interests) VALUES (?,?,?,?,?,?)", 
-                        generatedKeys)){
-            
-
-            pstmt.setString(1,form.getUsername());
-            pstmt.setString(2, form.getEmail());
-            pstmt.setString(3, form.getPassword());
-            pstmt.setString(4, form.getBirthday());
-            pstmt.setString(5, form.getLocation());
-            pstmt.setString(6, form.getInterests());
-            pstmt.executeUpdate();
-            
-            ResultSet rs = pstmt.getGeneratedKeys();
-            while(rs.next()) {
-                form.setId(rs.getLong(1));
-            }
-        }
-        catch(Exception ex) {
-            throw new ServletException(ex.getMessage());
-        }
     }
 
     /**
@@ -72,22 +45,36 @@ public class CreateServlet3 extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
         response.getWriter().append("Served at: ").append(request.getContextPath());
-        UserData form = new UserData();
-        form.setUsername(request.getParameter("username"));
-        form.setEmail(request.getParameter("email"));
-        form.setPassword(request.getParameter("password"));
-        form.setBirthday(request.getParameter("birthday"));
-        form.setLocation(request.getParameter("location"));
-        form.setInterests(request.getParameter("interests"));
-      //  form.setQuantity(Integer.valueOf(request.getParameter("quantity")));
-        persist(form);
+        
+        UserBean user = new UserBean();
+        user.setUsername(request.getParameter("username"));
+        user.setEmail(request.getParameter("email"));
+        user.setPassword(request.getParameter("password"));
+        user.setBirthday(request.getParameter("birthday"));
+        user.setLocation(request.getParameter("location"));
+        user.setInterests(request.getParameter("interests"));
+        Part filePart = request.getPart("photo");
+        user.setFilename(filePart.getSubmittedFileName());
+        
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                InputStream in = filePart.getInputStream()){
+                int i = 0;
+                while ((i = in.read()) != -1) {
+                    baos.write(i);
+                }
+                user.setPhoto(baos.toByteArray());
+                baos.flush();
+            }
+            catch(IOException ex) {
+                throw new ServletException(ex.getMessage());
+            }
+        persist(user);
         
         final HttpSession session = request.getSession();
-        session.setAttribute("form", form);
+        session.setAttribute("user", user);
         
-        final RequestDispatcher disp = request.getRequestDispatcher("jsp/Create/NextPage.jsp");
+        final RequestDispatcher disp = request.getRequestDispatcher("jsp/Create/SaveData.jsp");
         disp.forward(request, response);
-        
     }
 
     /**
@@ -98,6 +85,26 @@ public class CreateServlet3 extends HttpServlet {
         doGet(request, response);
     }
     
-    
+    public void persist(UserBean user) throws ServletException {
+        try (Connection con = ds.getConnection(); PreparedStatement pstmt = con.prepareStatement("INSERT INTO users (username,email,password,birthday,location,interests,photo) VALUES (?,?,?,?,?,?,?)")){
+            pstmt.setString(1,user.getUsername());
+            pstmt.setString(2, user.getEmail());
+            pstmt.setString(3, user.getPassword());
+            pstmt.setString(4, user.getBirthday());
+            pstmt.setString(5, user.getLocation());
+            pstmt.setString(6, user.getInterests());
+            //pstmt.setString(7, user.getFilename());
+            pstmt.setBytes(7, user.getPhoto());
+            pstmt.executeUpdate();
+            
+        }
+        catch(Exception ex) {
+            throw new ServletException(ex.getMessage());
+        }
+        
+    }
 
 }
+
+
+
